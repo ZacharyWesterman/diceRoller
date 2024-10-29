5343999f86c1bbbbb8bc514b1c459ca134242b7b
Joe P Shoulak <joepshoulak2@gmail.com>
2024-10-29 18:29:29 -0500

    comments

--- main.py
+++ main.py
@@ -7,21 +7,17 @@ diceRollRegex = r'\d*d\d+'
 
 # Rolls [number]d[sides], like 2d6, 5d4, or even d100 
 def roll(sides, number=1):
-  def sumRolls(total, _):
-    return total + randint(1, sides)
-
+  sumRolls = lambda total, _: total + randint(1, sides)
   return reduce(sumRolls, range(number), 0)
 
 # Converts the regex of kdn into the result of the roll
 def regexToRollResult(match):
   num, sides = match.group().split("d")
-
   return str(roll(int(sides), int(num) if num else 1))
 
 # Takes an input dice string and returns the result of the relevant dice and math
 def rollDiceString(diceString):
   subbedString = sub(diceRollRegex, regexToRollResult, diceString)
-
   return eval(subbedString)
 
 ###### Testing ######
@@ -33,6 +29,7 @@ def testDiceString(diceString):
   total, quantity = 0, 100000
   for _ in range(quantity):
     total += int(rollDiceString(diceString))
+  
   print(f"Ao{quantity}: {total/quantity}\n")
 
 ###### Main ######
